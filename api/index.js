8bfaa237e0e93e2b53048117b8ee501b025d2d15
unknown <ahmedjibran73@gmail.com>
2023-08-30 15:42:06 +0500

    hell with ths vercel

--- api/index.js
+++ api/index.js
@@ -4,14 +4,11 @@ const swaggerDocument = require("../swagger.json");
 const express = require("express");
 require("dotenv").config();
 require("./mongo");
-const TransactionRoutes = require("./src/Routes/Transaction");
 const UserRoutes = require("./src/Routes/User");
 const MsgRoutes = require("./src/Routes/Message");
 const dexApiRoutes = require("./src/Routes/dexapi");
 const app = express();
 app.use(express.json());
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Running on port ${process.env.PORT}`);
