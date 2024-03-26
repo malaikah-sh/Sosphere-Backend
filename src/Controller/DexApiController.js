58c0141e43003dba7e225171b0be725b973c666f
unknown <ahmedjibran73@gmail.com>
2024-03-26 21:55:04 +0500

    v2

--- src/Controller/DexApiController.js
+++ src/Controller/DexApiController.js
@@ -36,8 +36,26 @@ const getPair = async (req, res) => {
     res.status(error.response.status).json(error.response.data);
   }
 };
+const getV2Token = async (req, res) => {
+  try {
+    const response = await axios.get("https://api.dextools.io/v2/token/audit", {
+      params: {
+        chain: req.query.chain,
+        address: req.query.address,
+      },
+      headers: {
+        "x-api-key": "c471bc4c8b5f0053cf4c26c0d5af5ccf", // Replace with your API key
+        "Access-Control-Allow-Origin": "true",
+      },
+    });
+    return res.json(response.data.data);
+  } catch (error) {
+    res.status(error.response.status).json(error.response.data);
+  }
+};
 
 module.exports = {
   getToken,
   getPair,
+  getV2Token,
 };
--- src/Routes/dexapi.js
+++ src/Routes/dexapi.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/token", controller.getToken);
 router.get("/pair", controller.getPair);
+router.get("/v2/token", controller.getV2Token);
 
 module.exports = router;
