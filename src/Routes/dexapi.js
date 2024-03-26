f67be017a7989c935da90ab9f9bb762042ab87cb
unknown <ahmedjibran73@gmail.com>
2024-03-26 22:21:31 +0500

    v2 cancel

--- src/Routes/dexapi.js
+++ src/Routes/dexapi.js
@@ -5,6 +5,5 @@ const router = express.Router();
 
 router.get("/token", controller.getToken);
 router.get("/pair", controller.getPair);
-router.get("/v2/token", controller.getV2Token);
 
 module.exports = router;
